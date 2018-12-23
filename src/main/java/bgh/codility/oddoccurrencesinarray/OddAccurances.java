2b736115ae99018a66c7c20f4b7728616dee8d83
B Gh <11256168+bethygh@users.noreply.github.com>
2018-12-23 14:44:41 +0330

    make OddAccurances simple and time efficient

--- src/main/java/bgh/codility/oddoccurrencesinarray/OddAccurances.java
+++ src/main/java/bgh/codility/oddoccurrencesinarray/OddAccurances.java
@@ -6,28 +6,13 @@ import java.util.Map;
 public class OddAccurances {
 
     public int findOddAccurances (int[] input) {
-        if (input.length%2 == 0)
-            return -1;
 
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int odd  =0 ;
 
         for (int i = 0 ; i< input.length ; i++) {
-            if (map.containsKey(input[i])) {
-                map.put(input[i],  map.get(input[i])+1);
-            }
-            else {
-                map.put(input[i],  1);
-            }
+            odd ^= input[i];
         }
 
-        if (! map.containsValue(1) )
-            return -1;
-
-        return map.entrySet()
-                .stream()
-                .filter((i) -> (i.getValue()%2 != 0))
-//                .min((x, y) -> Integer.compare(x.getValue(), y.getValue()))
-                .findFirst()
-                .get().getKey();
+       return odd;
     }
 }
