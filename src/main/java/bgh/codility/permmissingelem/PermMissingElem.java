0a4d0c303920a3b3b7759ee08ee34e3409d4b795
B Gh <11256168+bethygh@users.noreply.github.com>
2018-12-23 10:54:28 +0330

    simplifying PermMissingElem

--- src/main/java/bgh/codility/permmissingelem/PermMissingElem.java
+++ src/main/java/bgh/codility/permmissingelem/PermMissingElem.java
@@ -9,38 +9,18 @@ public class PermMissingElem {
         if (input.length == 0)
             return 1;
 
-        int[] sorted = IntStream.of(input)
+        int[] sorted = java.util.stream.IntStream.of(input)
                 .boxed()
                 .sorted()
                 .mapToInt(i -> i)
                 .toArray();
 
-        return missingItem(sorted, 0);
-    }
-
-    private int missingItem(int[] input, int shiftVal) {
-
-        int midIdx = (input.length / 2 );
-
-        /*if (input.length == 1)
-            return input[0]-1;*/
-        if (input.length == 1) {
-            if (input[0] == (shiftVal + 2)) return 1 + shiftVal;//TODO
-            if (input[0] == (shiftVal + 1)) return 2 + shiftVal;//TODO
-        }
-        if (input.length == 2) {
-            if (input[0] == (shiftVal + 2)) return 1 + shiftVal;//TODO
-            if (input[0] == (shiftVal + 1))
-                if (input[1] == (shiftVal + 2)) return 3 + shiftVal;
-                else return 2 + shiftVal;//TODO
+        for(int i=0 ; i<sorted.length ; i++)
+        {
+            if (sorted[i] != i+1)
+                return sorted[i] - 1;
         }
 
-        if (input[midIdx] == (shiftVal+midIdx+1)) {
-            return missingItem(Arrays.copyOfRange(input, midIdx + 1, shiftVal+input.length), input[midIdx]);
-        }
-        else {
-            return missingItem(Arrays.copyOfRange(input, 0, midIdx+1), shiftVal);
-        }
-
+        return sorted.length+1;
     }
 }
