47f90152095e9e10e78a9ce8c6dcbd8ce0d53586
B Gh <11256168+bethygh@users.noreply.github.com>
2018-12-23 13:36:37 +0330

    improve time efficiency on TapeEquilibrium

--- src/main/java/bgh/codility/tapeequilibrium/TapeEquilibrium.java
+++ src/main/java/bgh/codility/tapeequilibrium/TapeEquilibrium.java
@@ -9,13 +9,15 @@ public class TapeEquilibrium {
         if (input.length == 1) return input[0];
 
         int min = Integer.MAX_VALUE, diff;
-        long a, b;
+        long a, b=0;
+        a = Arrays.stream(input).sum();
         for (int p=1 ; p<input.length ; p++) {
-            a = Arrays.stream(Arrays.copyOfRange(input, 0, p)).sum();
-            b = Arrays.stream(Arrays.copyOfRange(input, p, input.length)).sum();
+            b += input[p-1];
+            a -= input[p-1];
 
             diff = (int) Math.abs(a-b);
             min = (min<diff) ? min : diff;
+            if(min == 0) return min;
         }
 
         return min;
